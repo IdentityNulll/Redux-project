89e10d609d3d4a0bd477fac67f5e353b29d84e78
Identitynull <identitynulll@gmail.com>
2026-02-03 15:57:14 +0500

    working with redux

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -22,7 +22,7 @@ const authSlice = createSlice({
       state.user = null
       state.token = null
       state.isAuthenticated = false
-      localStorage.removeItem('token')
+      localStorage.clear()
     },
   },
 })
